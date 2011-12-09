5f92643364edaf4b3d52ef2c5df063ba897d7623
Robert Ross <robert@maintainedauto.com>
2011-12-08 21:32:07 -0800

    Add connection specs.

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -26,4 +26,22 @@ describe Gibbons::Base do
 		base = Gibbons::Base.new
 		base.connection.site.should eq('http://www.google.com')
 	end
+
+	it ".connection should return a Gibbons::Connection object with prefix" do
+		Gibbons::Base.site   = 'http://www.google.com'
+		Gibbons::Base.prefix = '/'
+		Gibbons::Base.format = :json
+
+		base = Gibbons::Base.new
+		base.connection.prefix.should eq('/')
+	end
+
+	it ".connection should return a Gibbons::Connection object with format" do
+		Gibbons::Base.site   = 'http://www.google.com'
+		Gibbons::Base.prefix = '/'
+		Gibbons::Base.format = :json
+
+		base = Gibbons::Base.new
+		base.connection.format.should eq(:json)
+	end
 end
