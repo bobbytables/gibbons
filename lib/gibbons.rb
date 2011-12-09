51a5ef4001dd1ecb920c2a3f5cba06a74b1876f4
Robert Ross <robert@maintainedauto.com>
2011-12-08 21:04:08 -0800

    Add basic setup.

--- lib/gibbons.rb
+++ lib/gibbons.rb
@@ -1,7 +1,14 @@
 require "gibbons/version"
 
 require 'active_support/concern'
+require 'acctive_support/attribute_accessors'
+require 'httparty'
+require 'hashie'
 
 module Gibbons
-  # Your code goes here...
+  class << self
+  	def configure(&block)
+  		Gibbons::Base.configure(&block)
+  	end
+  end
 end
