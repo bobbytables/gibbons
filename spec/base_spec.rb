8d8e1a6ea56a2ba5c144c38a6279e398e10939c7
Robert Ross <robert@maintainedauto.com>
2012-01-26 18:42:43 -0800

    Change to spaces from tabs.

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,75 +1,75 @@
 require 'spec_helper'
 
 describe Gibbons::Base do
-	it "should set a logger" do
-		Gibbons::Base.should respond_to :logger=
-	end
+  it "should set a logger" do
+    Gibbons::Base.should respond_to :logger=
+  end
 
-	it "should return a logger" do
-		Gibbons::Base.should respond_to :logger
-	end
+  it "should return a logger" do
+    Gibbons::Base.should respond_to :logger
+  end
 
-	describe "Connections" do
-		it ".connection should return a Gibbons::Connection object" do
-			Gibbons::Base.site   = 'http://www.google.com'
-			Gibbons::Base.prefix = '/'
-			Gibbons::Base.format = :json
+  describe "Connections" do
+    it ".connection should return a Gibbons::Connection object" do
+      Gibbons::Base.site   = 'http://www.google.com'
+      Gibbons::Base.prefix = '/'
+      Gibbons::Base.format = :json
 
-			base = Gibbons::Base.new
-			base.connection.should be_kind_of Gibbons::Connection
-		end
+      base = Gibbons::Base.new
+      base.connection.should be_kind_of Gibbons::Connection
+    end
 
-		it ".connection should return a Gibbons::Connection object with site" do
-			Gibbons::Base.site   = 'http://www.google.com'
-			Gibbons::Base.prefix = '/'
-			Gibbons::Base.format = :json
+    it ".connection should return a Gibbons::Connection object with site" do
+      Gibbons::Base.site   = 'http://www.google.com'
+      Gibbons::Base.prefix = '/'
+      Gibbons::Base.format = :json
 
-			base = Gibbons::Base.new
-			base.connection.site.should eq('http://www.google.com')
-		end
+      base = Gibbons::Base.new
+      base.connection.site.should eq('http://www.google.com')
+    end
 
-		it ".connection should return a Gibbons::Connection object with prefix" do
-			Gibbons::Base.site   = 'http://www.google.com'
-			Gibbons::Base.prefix = '/'
-			Gibbons::Base.format = :json
+    it ".connection should return a Gibbons::Connection object with prefix" do
+      Gibbons::Base.site   = 'http://www.google.com'
+      Gibbons::Base.prefix = '/'
+      Gibbons::Base.format = :json
 
-			base = Gibbons::Base.new
-			base.connection.prefix.should eq('/')
-		end
+      base = Gibbons::Base.new
+      base.connection.prefix.should eq('/')
+    end
 
-		it ".connection should return a Gibbons::Connection object with format" do
-			Gibbons::Base.site   = 'http://www.google.com'
-			Gibbons::Base.prefix = '/'
-			Gibbons::Base.format = :json
+    it ".connection should return a Gibbons::Connection object with format" do
+      Gibbons::Base.site   = 'http://www.google.com'
+      Gibbons::Base.prefix = '/'
+      Gibbons::Base.format = :json
 
-			base = Gibbons::Base.new
-			base.connection.format.should eq(:json)
-		end
-	end
+      base = Gibbons::Base.new
+      base.connection.format.should eq(:json)
+    end
+  end
 
-	describe "Configuration" do
-		it "should configure site with a block" do
-			Gibbons::Base.configure do |config|
-				config.site   = 'http://www.creativequeries.com'
-			end
+  describe "Configuration" do
+    it "should configure site with a block" do
+      Gibbons::Base.configure do |config|
+        config.site   = 'http://www.creativequeries.com'
+      end
 
-			Gibbons::Base.site.should eq('http://www.creativequeries.com')
-		end
+      Gibbons::Base.site.should eq('http://www.creativequeries.com')
+    end
 
-		it "should configure prefix with a block" do
-			Gibbons::Base.configure do |config|
-				config.prefix = '/stuff'
-			end
+    it "should configure prefix with a block" do
+      Gibbons::Base.configure do |config|
+        config.prefix = '/stuff'
+      end
 
-			Gibbons::Base.prefix.should eq('/stuff')
-		end
+      Gibbons::Base.prefix.should eq('/stuff')
+    end
 
-		it "should configure format with a block" do
-			Gibbons::Base.configure do |config|
-				config.format = :json
-			end
+    it "should configure format with a block" do
+      Gibbons::Base.configure do |config|
+        config.format = :json
+      end
 
-			Gibbons::Base.format.should eq(:json)
-		end
-	end
+      Gibbons::Base.format.should eq(:json)
+    end
+  end
 end
