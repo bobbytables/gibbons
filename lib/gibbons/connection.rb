871a27123e3fbbe0e2d95ac063a8f594401e78d2
Robert Ross <robert@maintainedauto.com>
2012-01-26 18:40:08 -0800

    Remove tabs, replace with spaces.

--- lib/gibbons/connection.rb
+++ lib/gibbons/connection.rb
@@ -1,12 +1,11 @@
 module Gibbons
-	class Connection
-		include HTTParty
-		attr_accessor :site, :prefix, :format
+  class Connection
+    attr_accessor :site, :prefix, :format
 
-		def initialize(options={})
-			self.site   = options[:site] if options[:site]
-			self.prefix = options[:prefix] if options[:prefix]
-			self.format = options[:format].to_sym if options[:format]
-		end
-	end
+    def initialize(options={})
+      self.site   = options[:site] if options[:site]
+      self.prefix = options[:prefix] if options[:prefix]
+      self.format = options[:format].to_sym if options[:format]
+    end
+  end
 end
