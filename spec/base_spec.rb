51e2042640dbe739bdb0e0df212b96d8546b34a4
Robert Ross <robert@maintainedauto.com>
2012-01-26 18:42:05 -0800

    Spacing for base spec.

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -9,39 +9,67 @@ describe Gibbons::Base do
 		Gibbons::Base.should respond_to :logger
 	end
 
-	it ".connection should return a Gibbons::Connection object" do
-		Gibbons::Base.site   = 'http://www.google.com'
-		Gibbons::Base.prefix = '/'
-		Gibbons::Base.format = :json
+	describe "Connections" do
+		it ".connection should return a Gibbons::Connection object" do
+			Gibbons::Base.site   = 'http://www.google.com'
+			Gibbons::Base.prefix = '/'
+			Gibbons::Base.format = :json
 
-		base = Gibbons::Base.new
-		base.connection.should be_kind_of Gibbons::Connection
+			base = Gibbons::Base.new
+			base.connection.should be_kind_of Gibbons::Connection
+		end
+
+		it ".connection should return a Gibbons::Connection object with site" do
+			Gibbons::Base.site   = 'http://www.google.com'
+			Gibbons::Base.prefix = '/'
+			Gibbons::Base.format = :json
+
+			base = Gibbons::Base.new
+			base.connection.site.should eq('http://www.google.com')
+		end
+
+		it ".connection should return a Gibbons::Connection object with prefix" do
+			Gibbons::Base.site   = 'http://www.google.com'
+			Gibbons::Base.prefix = '/'
+			Gibbons::Base.format = :json
+
+			base = Gibbons::Base.new
+			base.connection.prefix.should eq('/')
+		end
+
+		it ".connection should return a Gibbons::Connection object with format" do
+			Gibbons::Base.site   = 'http://www.google.com'
+			Gibbons::Base.prefix = '/'
+			Gibbons::Base.format = :json
+
+			base = Gibbons::Base.new
+			base.connection.format.should eq(:json)
+		end
 	end
 
-	it ".connection should return a Gibbons::Connection object with site" do
-		Gibbons::Base.site   = 'http://www.google.com'
-		Gibbons::Base.prefix = '/'
-		Gibbons::Base.format = :json
+	describe "Configuration" do
+		it "should configure site with a block" do
+			Gibbons::Base.configure do |config|
+				config.site   = 'http://www.creativequeries.com'
+			end
 
-		base = Gibbons::Base.new
-		base.connection.site.should eq('http://www.google.com')
-	end
+			Gibbons::Base.site.should eq('http://www.creativequeries.com')
+		end
 
-	it ".connection should return a Gibbons::Connection object with prefix" do
-		Gibbons::Base.site   = 'http://www.google.com'
-		Gibbons::Base.prefix = '/'
-		Gibbons::Base.format = :json
+		it "should configure prefix with a block" do
+			Gibbons::Base.configure do |config|
+				config.prefix = '/stuff'
+			end
 
-		base = Gibbons::Base.new
-		base.connection.prefix.should eq('/')
-	end
+			Gibbons::Base.prefix.should eq('/stuff')
+		end
 
-	it ".connection should return a Gibbons::Connection object with format" do
-		Gibbons::Base.site   = 'http://www.google.com'
-		Gibbons::Base.prefix = '/'
-		Gibbons::Base.format = :json
+		it "should configure format with a block" do
+			Gibbons::Base.configure do |config|
+				config.format = :json
+			end
 
-		base = Gibbons::Base.new
-		base.connection.format.should eq(:json)
+			Gibbons::Base.format.should eq(:json)
+		end
 	end
 end
