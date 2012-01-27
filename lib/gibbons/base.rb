419c15d303927b2169966efaac5052915462d5b3
Robert Ross <robert@maintainedauto.com>
2012-01-26 18:39:39 -0800

    Refactor connection to memoize.

--- lib/gibbons/base.rb
+++ lib/gibbons/base.rb
@@ -1,17 +1,19 @@
 module Gibbons
-	class Base
-		cattr_accessor :logger
+  class Base
+    cattr_accessor :logger
 
-		cattr_accessor :configuration
-		@@configuration = {}
+    class_attribute :site
+    class_attribute :prefix
+    class_attribute :format
 
-		class_attribute :site
-		class_attribute :prefix
-		class_attribute :format
+    class << self
+      def configure(&block)
+        yield self
+      end
+    end
 
-		def connection
-			@connection = Connection.new(:site => self.site, :prefix => self.prefix, :format => self.format)
-			@connection
-		end
-	end
+    def connection
+      @connection ||= Connection.new(:site => self.site, :prefix => self.prefix, :format => self.format)
+    end
+  end
 end
