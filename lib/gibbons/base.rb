6f0ece39cc0db675c9836e6fa56dd01a4bff5820
Robert Ross <robert@maintainedauto.com>
2012-01-30 10:47:07 -0800

    Memoize Connection method. Add method missing to generate request objects.

--- lib/gibbons/base.rb
+++ lib/gibbons/base.rb
@@ -10,10 +10,17 @@ module Gibbons
       def configure(&block)
         yield self
       end
-    end
 
-    def connection
-      @connection ||= Connection.new(:site => self.site, :prefix => self.prefix, :format => self.format)
+      def method_missing(method, *args, &block)
+        if Request.instance_methods.include? method
+          request = Request.new(@connection)
+          return request.send(method, *args)
+        end
+      end
+
+      def connection
+        @connection ||= Connection.new(:site => self.site, :prefix => self.prefix, :format => self.format)
+      end
     end
   end
 end
